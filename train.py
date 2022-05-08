1cdd5dddcfdbc8bb93e1d97c8db9e69de873ee39
nicolas.indjein <nicolas.indjein@epita.fr>
2022-05-09 00:42:45 +0200

    refactor: remove some imports

--- train.py
+++ train.py
@@ -1,14 +1,10 @@
 import cv2
-import re
 import joblib
 import numpy as np
 from math import copysign, log10
 import imageio
 import joblib
 import os.path
-import requests
-import tarfile
-import shutil
 from sklearn.model_selection import train_test_split
 from sklearn.cluster import MiniBatchKMeans
 from sklearn.pipeline import Pipeline, FeatureUnion
@@ -70,48 +66,7 @@ class HuMoments():
             
         return huMoments
 
-def init_resources(force = 0):
-    PATH_TO_RESOURCES = "../resources"
-    
-    if (os.path.exists(PATH_TO_RESOURCES) and os.path.isdir(PATH_TO_RESOURCES)):
-        if (not force):
-            print("Folder 'resources' found.")
-            return
-        else:
-            shutil.rmtree(PATH_TO_RESOURCES)
-    
-    # Create folder
-    os.mkdir(PATH_TO_RESOURCES)
-    print("[1/4] Folder '../resources' created.")
-    
-    # Download data
-    url_data = 'https://www.lrde.epita.fr/~jchazalo/SHARE/dobble-symbols-dataset-train.tar.gz'
-    url_dobble_img = 'https://boardgamereview.co.uk/wp-content/uploads/2020/02/Dobble-cards-pile-1.png'
-    TAR_PATH = os.path.join(PATH_TO_RESOURCES, 'dobble-symbols-dataset-train.tar.gz')
-    IMG_PATH = os.path.join(PATH_TO_RESOURCES, 'dobble_ref.png')
-    response = requests.get(url_data, stream=True)
-    if response.status_code == 200:
-        with open(TAR_PATH, 'wb') as f:
-            f.write(response.raw.read())
-    response = requests.get(url_dobble_img, stream=True)
-    if response.status_code == 200:
-        with open(IMG_PATH, 'wb') as f:
-            f.write(response.raw.read())
-            print("[2/4] Resources files uploaded.")
-    
-    # Extract from tar
-    tar = tarfile.open(TAR_PATH)
-    tar.extractall(PATH_TO_RESOURCES) # specify which folder to extract to
-    tar.close()
-    print("[3/4] Resources files extracted.")
-    
-    # Delete dowloaded tar
-    os.remove(TAR_PATH)
-    print("[4/4] Tarball removed.")
-
 if __name__ == "__main__":
-    init_resources(1)
-
     RANDOM_STATE = 42
 
     X = []
